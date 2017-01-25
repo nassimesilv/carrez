6916ebfc47b3d1a46a57e57a700f5ffd027846ef
Nassim <nassim.dahmani@outlook.fr>
2017-01-25 14:36:25 +0100

    Fix merges

--- public/js/leboncoin.js
+++ public/js/leboncoin.js
@@ -3,18 +3,6 @@ var fs      = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 
-<<<<<<< HEAD
-=======
-function cleanData(text){
-  text.replace(/(\r\n|\n|\r)/gm,"");
-  text=text.trim();
-  return text;
-}
-
-app.get('/scrape', function(req, res){
-  var title;
-    url = 'https://www.leboncoin.fr/ventes_immobilieres/1076257949.htm?ca=12_s';
->>>>>>> ae80471d42271ffd55e7f993de10b7352c57ca0d
 
 function cleanData(text){
   text.replace(/(\r\n|\n|\r)/gm,"");
@@ -29,40 +17,31 @@ module.exports = {compute :function(url){
 request(url, function(error, response, html){
         if(!error){
             var $ = cheerio.load(html);
+            var price, city, type,surface;
             var json = { price : "", city : "", type : "", surface:""};
 
             $('.item_price.clearfix').filter(function(){
                 var data = $(this);
-                json.price = data.attr('content');
+                price = data.attr('content');
+                json.price = price;
             })
 
             // get data
             $('.properties.lineNegative').find('div.line > h2 > span.value').each(function(i, elm) {
-                var data=$(this);
-                var prop=data.prev();
-                if(prop.text()=="Type de bien"){
-                    json.type=data.text();
+                //console.log($(this).text());
+                if($(this).prev().text()=="Type de bien"){
+                    json.type=$(this).text();
                 }
-<<<<<<< HEAD
                 else if ($(this).prev().children().first().text()=="Ville") {
                   json.city=cleanData($(this).text());
-=======
-                else if (prop.children().first().text()=="Ville") {
-                  json.city=cleanData(data.text());
->>>>>>> ae80471d42271ffd55e7f993de10b7352c57ca0d
                 }
-                else if (prop.text()=="Surface") {
-                  json.surface=data.text();
+                else if ($(this).prev().text()=="Surface") {
+                  json.surface=$(this).text();
                 }
+
             });
 
-<<<<<<< HEAD
             fs.writeFile('json\\leboncoin.json', JSON.stringify(json, null, 4))
-=======
-            fs.writeFile('..\\json\\leboncoin.json', JSON.stringify(json, null, 4), function(err){
-                res.send('File successfully written! - Check your project directory for the output.json file');
-            })
->>>>>>> ae80471d42271ffd55e7f993de10b7352c57ca0d
         }
     })
 
